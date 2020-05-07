85b1bf18cb57299ece42013f733114d3d0a9d2ec
luca hsieh <chungju.hsieh.ca@gmail.com>
2020-05-07 14:39:29 -0700

    template completed

--- src/_pages/property/property.page.js
+++ src/_pages/property/property.page.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { Component, createRef } from 'react'
 
-import { Card, Grid, Container, List, Divider, Header, Icon, Button } from 'semantic-ui-react'
+import {
+    Card, Grid, Container, List, Divider, Header, Icon, Button, Label, Form, Sticky, Rail,
+    Ref,
+} from 'semantic-ui-react'
+import { Formik } from 'formik'
+import * as Yup from "yup"
 
 import CarouselComponent from './carousel.component'
+import * as images from '../../_constants/images'
 
 class PropertyPage extends React.Component {
 
@@ -14,6 +20,9 @@ class PropertyPage extends React.Component {
     //     let { id } = useParams();
     //     this.setState({ propertyId: id });
     // }
+
+    contextRef = createRef()
+
 
     renderDivider(icon, text) {
         return (
@@ -81,45 +90,141 @@ class PropertyPage extends React.Component {
         )
     }
 
+    renderDivdingHeader(icon, text) {
+        return (
+            <Header icon={icon} as='h3' dividing>
+                {/* <Icon Icon name={icon} /> */}
+                {text}
+            </Header>
+        )
+    }
+
+    renderForm() {
+        return (
+            <Formik
+                initialValues={{ checkinDate: "", checkoutDate: "" }}
+                onSubmit={(values, { setSubmitting }) => {
+                    // submitting event here.
+
+                }}
+                validationSchema={Yup.object().shape({
+                    checkinDate: Yup.string()
+                        .required("Required"),
+                    checkoutDate: Yup.string()
+                        .required("No password provided.")
+                })}
+            >
+                {props => {
+                    const {
+                        values,
+                        touched,
+                        errors,
+                        isSubmitting,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit
+                    } = props;
+                    return (
+                        <div>
+                            <Form onSubmit={handleSubmit}>
+                                <div>
+                                    <label htmlFor="checkinDate">Check-in</label>
+                                    <input
+                                        name="checkinDate"
+                                        type="date"
+                                        placeholder={new Date().toDateString()}
+                                        value={values.checkinDate}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        className={errors.checkinDate && touched.checkinDate && "error"} />
+                                    {errors.checkinDate && touched.checkinDate && (
+                                        <Label basic color='red' pointing>
+                                            {errors.checkinDate}
+                                        </Label>
+                                    )}
+                                </div>
+                                <Divider />
+                                <div>
+
+                                    <label htmlFor="checkoutDate">Check-out</label>
+                                    <input
+                                        name="checkoutDate"
+                                        type="date"
+                                        placeholder={(new Date()).toDateString()}
+                                        value={values.checkoutDate}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        className={errors.checkoutDate && touched.checkoutDate && "error"}
+                                    />
+                                    {errors.checkoutDate && touched.checkoutDate && (
+                                        <Label basic color='red' pointing>
+                                            {errors.checkoutDate}
+                                        </Label>
+                                    )}
+                                </div>
+                                <Divider />
+                                <Button type="submit" >Book Now</Button>
+                            </Form>
+                        </div>
+                    )
+                }}
+
+            </Formik>
+        )
+    }
+
     render() {
         return (
             <div>
                 <CarouselComponent />
                 {this.rendernapshotInfo()}
 
-                <Grid>
-                    <Grid.Row>
-                        <Grid.Column width={10}>
-                            {this.renderDivider('home', 'Room Information')}
-                            {this.renderDivider('id badge', 'Host Information')}
+                <Container>
+                    <Ref innerRef={this.contextRef}>
 
-                        </Grid.Column>
-                        <Grid.Column width={6}>
-                            <Card>
-                                <Card.Content>
-                                    <Card.Header>$1000 <span className="font-weight-light">/month</span></Card.Header>
-                                    <span className="font-weight-light">smaller text will go here.</span>
-                                    <Divider></Divider>
-                                    <List>
-                                        <List.Item>
-                                            <List.Icon circular color='green' name='clipboard check' size='large' verticalAlign='middle' />
-                                        </List.Item>
-                                        <List.Item>
-                                            <List.Content>
-                                                <List.Header>Verified Host</List.Header>
-                                                <List.Description >This host is verifed by HomeSafe by conducting host background check.</List.Description>
-                                            </List.Content>
-                                        </List.Item>
-                                    </List>
-                                    <Divider></Divider>
+                        <Grid>
+                            <Grid.Row>
+                                <Grid.Column width={10}>
 
-                                    <Button>Book Now</Button>
-                                </Card.Content>
-                            </Card>
-                        </Grid.Column>
+                                    {this.renderDivdingHeader('home', 'Room Information')}
+                            4 fully furnished rooms in shared home in Fort Langley available for rent. The home is on acreage with plenty of space for outdoor activities and is a short distance from Fort Langley. Master Bedroom is available and has en suite. Close to Trinity Western University, Walnut grove, and Golden Ears Bridge. Nearby schools include Topham Elementary, Ecole des Voyageurs, Alex Hope Elementary, and Walnut Grove Secondary School. Nearest public transit is a 20 minute walk.
+                            {this.renderDivdingHeader('id badge', 'Host Information')}
+                            4 fully furnished rooms in shared home in Fort Langley available for rent. The home is on acreage with plenty of space for outdoor activities and is a short distance from Fort Langley. Master Bedroom is available and has en suite. Close to Trinity Western University, Walnut grove, and Golden Ears Bridge. Nearby schools include Topham Elementary, Ecole des Voyageurs, Alex Hope Elementary, and Walnut Grove Secondary School. Nearest public transit is a 20 minute walk.
+                            {this.renderDivdingHeader('home', 'Map')}
+                                    <img src={images.TEMPLATE_IMAGE} />
+                                    {this.renderDivdingHeader('home', 'Points of Interests')}
 
-                    </Grid.Row>
-                </Grid>
+                                </Grid.Column>
+                                <Grid.Column width={6}>
+                                    <Rail>
+                                        <Sticky context={this.contextRef}>
+                                            <Card className="mt-3">
+                                                <Card.Content>
+                                                    <Card.Header>$1000 <span className="font-weight-light">/month</span></Card.Header>
+                                                    <span className="font-weight-light">smaller text will go here.</span>
+                                                    <Divider></Divider>
+
+
+                                                    <Header as="p" block>
+                                                        <Icon Icon circular color='green' name='clipboard check' size='small' verticalAlign='middle' />
+                                        Verified Host
+                                        <Header.Subheader>This host is verifed by HomeSafe by conducting host background check.</Header.Subheader>
+                                                    </Header>
+                                                    <Divider></Divider>
+                                                    {this.renderForm()}
+                                                </Card.Content>
+                                            </Card>
+                                        </Sticky>
+                                    </Rail>
+
+
+                                </Grid.Column>
+
+                            </Grid.Row>
+                        </Grid>
+                    </Ref>
+
+                </Container>
             </div>
 
         )
